89713d17213c869a1593753bb631529a081c8dce
mhowell1 <mhowell1@una.edu>
2024-12-14 00:09:41 -0600

    maybe work render?

--- app.js
+++ app.js
@@ -162,6 +162,6 @@ app.post('/register', async (req, res) => {
 
 // Start server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", ()=>{
+  console.log(`Server listening to port ${PORT}`);
 });
